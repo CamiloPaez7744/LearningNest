4191e9dacbf11ec895767d4ef87e2d1a4b6a5f1b
Camilo Páez <camiloapaez@ucundinamarca.edu.co>
2025-07-18 19:57:41 -0500

    Add Pokemon data structure and initial Pokemon list in 02-objects.ts

--- 01-TypeScript-intro/src/bases/01-types.ts
+++ 01-TypeScript-intro/src/bases/01-types.ts
@@ -1,3 +1,7 @@
 export const name: string = 'Capitan Jack Sparrow';
 export const age: number = 44;
 export const isCaptain: boolean = true;
+
+export const templateString: string = `Nombre: ${name}, Edad: ${age}, Is Captain: ${isCaptain}`;
+
+console.log(templateString);
