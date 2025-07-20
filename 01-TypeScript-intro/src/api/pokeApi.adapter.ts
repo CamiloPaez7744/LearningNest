387cc74606b1e6ae4ee5f3336de877b6fb476861
Camilo Páez <camiloapaez@ucundinamarca.edu.co>
2025-07-19 21:45:39 -0500

    Add Pokemon class with dependency injection for API adapters and implement getMoves method

---
+++ 01-TypeScript-intro/src/api/pokeApi.adapter.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+export interface HttpAdapter {
+    get<T>(url: string): Promise<T>;
+}
+
+export class PokeApiFetchAdapter implements HttpAdapter {
+    async get<T>(url: string): Promise<T> {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    }
+}
+
+export class PokeApiAdapter implements HttpAdapter {
+
+    private readonly axios = axios;
+
+    async get<T>(url: string): Promise<T> {
+        const { data } = await this.axios.get<T>(url);
+        return data;
+    }
+
+    async post<T>(url: string, body: any): Promise<T> {
+        const { data } = await this.axios.post<T>(url, body);
+        return data;
+    }
+
+    async patch<T>(url: string, body: any): Promise<T> {
+        const { data } = await this.axios.patch<T>(url, body);
+        return data;
+    }
+
+    async delete<T>(url: string): Promise<T> {
+        const { data } = await this.axios.delete<T>(url);
+        return data;
+    }
+
+}
